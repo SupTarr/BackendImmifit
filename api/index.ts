eec836bfac50afb5a9cf463e0dace305f4603c05
SupTarr <tatasuppakrit@gmail.com>
2025-05-11 19:27:55 +0700

    feat: add vercel deploy

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
-import app from '../src/index.js';
+import app from "../src/index.js";
 
-export const config = { runtime: 'edge' };
+export const config = { runtime: "edge" };
 
 export default async function handler(request: Request) {
   return app.fetch(request);
