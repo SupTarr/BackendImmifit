ba1fe03ad7be6d50c32528c481bb65194a420975
SupTarr <tatasuppakrit@gmail.com>
2025-05-15 12:53:30 +0700

    fix: remove unuse code

--- src/jwt/middleware.ts
+++ src/jwt/middleware.ts
@@ -1,6 +1,6 @@
 import { AccessTokenPayload } from "../auth/model.js";
 
-export const verifyJWT = async ({ jwtAccess, set, request, error }: any) => {
+export const verifyJWT = async ({ jwtAccess, request, error }: any) => {
   const authHeader = request.headers.get("authorization");
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return error(400);
